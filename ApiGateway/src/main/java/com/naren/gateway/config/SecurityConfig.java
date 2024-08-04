2eef8ea738ae0cddacfe083b55675d02ce15e1b0
narender <narendarmekala.9778@gmail.com>
2024-08-04 18:07:25 +0530

    Adding Security to Api-Gateway using OKTA

---
+++ ApiGateway/src/main/java/com/naren/gateway/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.naren.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.reactive.config.EnableWebFlux;
+
+@Configuration
+@EnableWebFlux
+public class SecurityConfig {
+
+	@Bean
+	public SecurityWebFilterChain  securityWebFilterChain(ServerHttpSecurity httpSecurity) {
+		
+		httpSecurity
+				.authorizeExchange()
+				.anyExchange()
+				.authenticated()
+				.and()
+				.oauth2Client()
+				.and()
+				.oauth2ResourceServer()
+				.jwt();
+		
+		
+		return httpSecurity.build();
+	}
+}
